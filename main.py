cff5b7295f0f339640338e4c11ef8936a136c067
MadanDaniil <madan.daniil@yandex.ru>
2023-12-05 21:05:19 +0200

    2й коммит

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+# Необходимо написать программу, которая будет считывать последовательности измерений.
+# В каждой последовательности нужно выбрать максимальное значение, а в итоге вывести отсортированный по убыванию список
+# этих макс значений, разделенных символом “;”.
+# Во входных данных в первой строке будет задано целое положительное число –
+# сколько записей нужно обработать, причем самих записей может быть больше чем это число, это нужно учесть.
+# Значения в рамках одной записи разделены пробелом, минимальное число значений в записи – 1.
+# Записи разделены переводом строки.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+cnt = input()
+lst = []
+i = 0
+while data := input():
+    i+=1
+    if(i <= int(cnt)):
+        lst.append(max(data))
+lst.sort(reverse=1)
+joined = ";".join(lst)
+print(joined)
